108d3fd245494bc9ba7a4325187ffa86a10e5f9c
Abrorbek abdulkhamidov <abdulkhamidovabrorbek@gmail.com>
2023-10-28 17:59:17 +0500

    runner update

--- runner/site.js
+++ runner/site.js
@@ -8,4 +8,10 @@ app.get("/", (req, res) => {
   res.sendFile(process.cwd() + "/../dist/index.html")
 })
 
+app.all("/*", (req, res) => {
+  res.status(400).send({
+    message : `Given url ${req.url} is not found`
+  })
+})
+
 app.listen(1001, () => console.log(`Portfolio site working on ${1001}`));
